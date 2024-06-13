2d07520ed24e9f7af1ac06621716099d7ed19560
yo16 <xxyo1xx@gmail.com>
2024-06-13 22:08:21 +0900

    add auth function

--- react-firebase-auth/src/components/SignUp.js
+++ react-firebase-auth/src/components/SignUp.js
@@ -1,6 +1,49 @@
+import { useState } from "react";
+import { createUser } from "../firebase";
 
 export function SignUp() {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+
+    function handleChangeEmail(event) {
+        setEmail(event.currentTarget.value);
+    }
+    function handleChangePass(event) {
+        setPass(event.currentTarget.value);
+    }
+
+    function handleOnRegist() {
+        createUser(email, pass);
+    }
+
     return (
-        <h1>ユーザー登録</h1>
+        <div>
+            <h1>ユーザー登録</h1>
+            <div>
+                <label>メールアドレス</label>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                    defaultValue={email}
+                    onChange={(e)=> {handleChangeEmail(e)}}
+                />
+            </div>
+            <div>
+                <label>パスワード</label>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    defaultValue={pass}
+                    onChange={(e)=> {handleChangePass(e)}}
+                />
+            </div>
+            <div>
+                <button
+                    onClick={handleOnRegist}
+                >登録</button>
+            </div>
+        </div>
     );
 }
